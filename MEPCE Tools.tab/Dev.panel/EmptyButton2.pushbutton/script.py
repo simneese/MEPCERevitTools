e5fdbd93d53a0f17f30b18e354482217c2606664
Simeon Neese <simneese@gmail.com>
2025-05-28 19:19:10 -0500

    Added empty button 2

---
+++ MEPCE Tools.tab/Dev.panel/EmptyButton2.pushbutton/script.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+__title__ = "Empty Button 2"
+__doc__ = """Version = 1.0
+Date    = 2025.05.28
+_________________________________________________________________
+Description:
+This script will create circuits and power lighting zones.
+_________________________________________________________________
+How-to:
+-> Draw filled regions around lighting zones
+-> Click Button
+-> Done!
+_________________________________________________________________
+Last update:
+- [2025.05.28] - 1.0 RELEASE
+_________________________________________________________________
+To-Do:
+- 
+_________________________________________________________________
+Author: Simeon Neese"""
+
+# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
+# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
+# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
+#==================================================
+# Regular + Autodesk
+from Autodesk.Revit.DB import *
+
+# pyRevit
+from pyrevit import revit, forms
+
+# .NET Imports (You often need List import)
+import clr
+clr.AddReference("System")
+from System.Collections.Generic import List
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
+#==================================================
+from Autodesk.Revit.UI import UIDocument
+doc   = __revit__.ActiveUIDocument.Document #type: Document
+uidoc = __revit__.ActiveUIDocument          #type: UIDocument
+app   = __revit__.Application               #type: Application
+
+# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+# ╠╣ ║ ║║║║║   ║ ║║ ║║║║╚═╗
+# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+#==================================================
+
+# ╔╦╗╔═╗╦╔╗╔
+# ║║║╠═╣║║║║
+# ╩ ╩╩ ╩╩╝╚╝ MAIN
+#==================================================
+# START CODE HERE
+print('Template has been developed by Erik Frits.')
